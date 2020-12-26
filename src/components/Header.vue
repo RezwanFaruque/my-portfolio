2464c3e9d544ad7792bb2ba8d2483fc1840a539c
RezwanFaruque <rezwanfaruque9@gmail.com>
2020-12-26 23:36:06 +0600

    setting up every dependency

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,15 @@
 <template>
   
   <div>
+
       <h1>This is header components</h1>
+       <div class="container">
+         <div class="row">
+            <div class="col-12">
+               <h2>This is test of boostrap</h2>
+            </div>
+         </div>
+       </div>
   </div>
 
 </template>
@@ -12,3 +20,4 @@ export default {
 
 }
 </script>
+
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+
+// loading bootstrap
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+// loading sass preprocessor
+import './assets/index.scss';
 
 Vue.config.productionTip = false
 
