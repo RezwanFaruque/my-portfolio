fa37fdaf64090ed32f4392d920729eb39324a90f
RezwanFaruque <rezwanfaruque9@gmail.com>
2020-12-27 18:21:24 +0600

    header-section-design ongoing

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,22 +1,52 @@
 <template>
   
   <div>
+      <section class="header d-flex">
+          <div class="left-side">
+             <div class="logo">
+               Portfolio
+             </div>
+             <div class="image-section">
+               <div class="image-outer-layer">
+                 <div class="image">
+                   <img :src="Myimage" alt="" srcset="">
+                 </div>
+               </div>
+             </div>
+             <div class="social-icons">
+               
+                <div class="all-social-me">
+                   <div class="icons facebook">
+                     <img src="../assets/images/facebook.png" alt="" srcset="">
+                   </div>
+                   <div class="icons twitter">
+                     <img src="../assets/images/twitter.png" alt="" srcset="">
+                   </div>
+                   <div class="icons instagram">
+                     <img src="../assets/images/instagram.png" alt="" srcset="">
+                   </div>
+                </div>
+             </div>
+          </div>
+         
+          <div class="right-side">
 
-      <h1>This is header components</h1>
-       <div class="container">
-         <div class="row">
-            <div class="col-12">
-               <h2>This is test of boostrap</h2>
-            </div>
-         </div>
-       </div>
+          </div>
+
+      </section>
   </div>
 
 </template>
 
 <script>
+import Myimage from '../assets/images/me.jpg';
 export default {
     name: 'HeaderComponent',
+    data:function(){
+      return{
+        Myimage: Myimage,
+      }
+    }
 
 }
 </script>
